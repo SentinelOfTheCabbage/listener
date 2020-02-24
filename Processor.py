f3e66b1716780e7f3234108337f67e34cf0ce343
Sentinelofthecabbage <nail-009@ya.ru>
2020-02-24 12:05:02 +0200

    Fitting Sample class

--- Processor.py
+++ Processor.py
@@ -1,3 +1,5 @@
+import Sample
+
 class Processor:
     def __init__(self,hz_scale):
         self.curr_notes = []
@@ -5,21 +7,26 @@ class Processor:
         self.hz_scale = hz_scale
 
     def calculate_data(self,sample):
-        max_values, max_indexes = sample.get_groups_max_values()
+        max_ind, max_val = sample.max_ind, sample.max_val
         risk_br = sample.get_risk_db_val()
         hard_br = sample.get_hard_db_val()
 
         for elem in curr_notes:
-            group_num = curr_group(elem.hz_history[-1])
-            elem.append_story(values[group_num], group_num)
-            
+            group_ind = curr_group(elem.hz_history[-1])
+            cur_ind   = sample.max_ind[group_ind]
+            cur_val   = sample.max_val[group_ind]
+            elem.append_story(cur_val, cur_ind)
+        
+        # Добавить список, хранящий индексы тех групп, которые ещё не 
+        # подверглись обходу процессора 
+
 
 class Note:
     def __init__(self, new_db, new_hz, start_sample):
         self.hz_history = []
         self.db_history = []
-        self.append_story(new_db,new_hz)
         self.start_sample = start_sample
+        self.append_story(new_db,new_hz)
 
     def append_story(self, new_db, new_hz_ind):
         self.hz_ind_history.append(new_hz_ind)
--- Sample.py
+++ Sample.py
@@ -1,14 +1,18 @@
 class Sample:
     def __init__(self, data):
         self.data = data
-        sample_glacer()
-        get_groups_sep()
+        self.sample_glacer()
+        self.separate_to_groups()
+        self.risk_db = self.data.mean()*3*0.8
+        self.hard_db = self.data.mean()*3
 
     def __glancing__(self, count, delta):
+        data = self.data
         if count > 0:
             for pos,elem in enumerate(data):
                 for i in range(1,delta+1):
-                    if (pos > i) and (len(data) - pos > i) and elem < data[pos - i] and elem < data[pos + i]:
+                    if ((pos > i) and (len(data) - pos > i) and elem < 
+                                    data[pos - i] and elem < data[pos + i]):
                         data[pos] = (data[pos - i] + data[pos + i])/2
             temp_db = glancing(data, count - 1, delta)
         return data
@@ -17,34 +21,39 @@ class Sample:
         self.data = self.__glancing__(5, 3)
         self.data = self.__glancing__(5, 2)
         self.data = self.__glancing__(5, 1)
-        return self.data
 
-    def get_groups_separators(self, hz_scale):
+    def separate__to_groups(self, hz_scale):
         self.sep_indexes = [0]
         for pos,elem in enumerate(self.data):
-            if (pos>0 and pos < len(data)-1) and (data[pos - 1] >= elem <= data[pos + 1]):
+            if (pos>0 and pos < len(data)-1) and 
+                                    (data[pos - 1] >= elem <= data[pos + 1]):
                 if self.sep_indexes [-1] == pos - 1:
                     self.sep_indexes [-1] = pos
                 else:
                     self.sep_indexes.append(pos)
+        self.__get_groups_max_values__()
         return self.sep_indexes
 
-    def get_groups_max_values(self, sep_group = self.sep_indexes):
-        self.indexes = []
-        self.values = []
-        for pos, border in enumerate(sep_group):
-            if pos < len(sep_group) - 1:
-                self.values.append(max(data[border: sep_group[pos + 1]]))
-                self.indexes.append(border + np.where(data[border: sep_group[pos + 1]] == (value[-1]))[0][0])
-        return self.values, self.indexes
+    def __get_groups_max_values__(self):
+        self.max_ind = []
+        self.max_val = []
+
+        for pos, bound in enumerate(self.sep_indexes):
+            cur_slice = self.data[bound: sep_indexes[pos + 1]]
+            if  pos < len(self.sep_indexes) - 1:
+                self.values.append(max(cur_slice))
+                self.indexes.append(bound + np.where(cur_slice == (value[-1])
+                                                        )[0][0])
+
+        return self.max_val, self.max_ind
 
     def get_risk_db_val(self):
-        return 3*self.data.mean()*0.8
+        return self.risk_db
 
     def get_hard_db_val(self):
-        return 3*self.data.mean()
+        return self.hard_db
 
     def curr_group(self, index):
-        for pos, i in enumerate(self.indexes):
+        for pos, i in enumerate(self.max_ind):
             if index < i:
                 return pos
